bb1ba5b2775ab1acb2cca8cd882c9b9a3a3d3996
17259202 <17259202@DSN1271058232.cfp127aluno.local>
2018-04-04 08:59:09 -0300

    Primeira versão

---
+++ Agenda/src/br/senai/sp/jandira/model/Contato.java
@@ -0,0 +1,57 @@
+package br.senai.sp.jandira.model;
+
+public class Contato {
+	
+	private int id;
+	private String nome;
+	private String dtNasc;
+	private String endereco;
+	private String telefone;
+	private String celular;
+	private String email;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDtNasc() {
+		return dtNasc;
+	}
+	public void setDtNasc(String dtNasc) {
+		this.dtNasc = dtNasc;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
