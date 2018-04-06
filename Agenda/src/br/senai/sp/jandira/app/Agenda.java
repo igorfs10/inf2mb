6a2ba39abef9c1b3f795693becf0cb63bc7a9ae3
17259202 <17259202@DSN1271058224.cfp127aluno.local>
2018-04-06 11:31:08 -0300

    Janela principal quase pronta

--- Agenda/src/br/senai/sp/jandira/app/Agenda.java
+++ Agenda/src/br/senai/sp/jandira/app/Agenda.java
@@ -1,9 +1,12 @@
 package br.senai.sp.jandira.app;
 
+import br.senai.sp.jandira.view.FrmAgenda;
+
 public class Agenda {
 
 	public static void main(String[] args) {
-	
+		FrmAgenda agenda = new FrmAgenda();
+		agenda.setVisible(true);
 	}
 
 }
--- Agenda/src/br/senai/sp/jandira/view/FrmAgenda.java
+++ Agenda/src/br/senai/sp/jandira/view/FrmAgenda.java
@@ -10,10 +10,17 @@ import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.ImageIcon;
+import javax.swing.border.TitledBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class FrmAgenda extends JFrame {
 
 	private JPanel painelPrincipal;
+	private JTable tabelaContatos;
+	JScrollPane scrollTabela;
+	JPanel painelTabela;
 
 	public FrmAgenda() {
 		setTitle("Agenda de contatos");
@@ -35,5 +42,57 @@ public class FrmAgenda extends JFrame {
 		lblAgenda.setFont(new Font("Arial", Font.BOLD, 28));
 		lblAgenda.setBounds(10, 11, 414, 44);
 		painelTitulo.add(lblAgenda);
+		
+		painelTabela = new JPanel();
+		painelTabela.setBorder(new TitledBorder(null, "Meus Contatos", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(51, 0, 204)));
+		painelTabela.setBounds(0, 64, 434, 197);
+		painelPrincipal.add(painelTabela);
+		painelTabela.setLayout(null);
+		
+		criarTabela();
+		
+		/*
+		tabelaContatos = new JTable();
+		tabelaContatos.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+			},
+			new String[] {
+				"ID", "NOME", "E-MAIL"
+			}
+		));
+		tabelaContatos.getColumnModel().getColumn(0).setPreferredWidth(45);
+		tabelaContatos.getColumnModel().getColumn(1).setPreferredWidth(261);
+		tabelaContatos.getColumnModel().getColumn(2).setPreferredWidth(308);
+		scrollTabela.setViewportView(tabelaContatos);
+		
+		*/
+	}
+	
+	public void criarTabela(){
+		DefaultTableModel modeloTabela = new DefaultTableModel();
+		Object[] linhas = new Object[3];
+		
+		int i = 0;
+		while(i < 5){
+			linhas [0] = "1";
+			linhas [1] = "Maria";
+			linhas [2] = "maria@teste.com.br";
+			modeloTabela.addRow(linhas);
+			i++;
+		}
+		String[] titulo = {"ID", "NOME", "E-MAIL"};
+		modeloTabela.setColumnIdentifiers(titulo);
+		
+		tabelaContatos.setModel(modeloTabela);
+		scrollTabela.setViewportView(tabelaContatos);
+		
+		scrollTabela = new JScrollPane();
+		scrollTabela.setBounds(10, 24, 414, 145);
+		painelTabela.add(scrollTabela);
 	}
 }
