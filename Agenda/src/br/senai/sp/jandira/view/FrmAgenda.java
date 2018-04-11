d9a81f3be249264bf4402b6bc6ef36e21af474b0
igorfs10 <igorfs10@gmail.com>
2018-04-11 09:38:37 -0300

    Adicionar icones. Icones feito por Freepik de www.flaticon.com

--- Agenda/src/br/senai/sp/jandira/view/FrmAgenda.java
+++ Agenda/src/br/senai/sp/jandira/view/FrmAgenda.java
@@ -14,6 +14,10 @@ import javax.swing.border.TitledBorder;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.UIManager;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.JButton;
 
 public class FrmAgenda extends JFrame {
 
@@ -25,7 +29,7 @@ public class FrmAgenda extends JFrame {
 	public FrmAgenda() {
 		setTitle("Agenda de contatos");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 390);
 		painelPrincipal = new JPanel();
 		painelPrincipal.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(painelPrincipal);
@@ -44,10 +48,36 @@ public class FrmAgenda extends JFrame {
 		painelTitulo.add(lblTituloTela);
 		
 		painelTabela = new JPanel();
-		painelTabela.setBorder(new TitledBorder(null, "Meus Contatos", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(51, 0, 204)));
-		painelTabela.setBounds(0, 64, 434, 197);
+		painelTabela.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Meus Contatos:", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(51, 0, 204)));
+		painelTabela.setBounds(10, 64, 414, 197);
 		painelPrincipal.add(painelTabela);
 		painelTabela.setLayout(null);
+		
+		JPanel painelBotoes = new JPanel();
+		painelBotoes.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		painelBotoes.setBounds(10, 272, 414, 66);
+		painelPrincipal.add(painelBotoes);
+		painelBotoes.setLayout(null);
+		
+		JButton btnNovo = new JButton("");
+		btnNovo.setIcon(new ImageIcon(FrmAgenda.class.getResource("/br/senai/sp/jandira/imagens/novo32.png")));
+		btnNovo.setBounds(10, 11, 44, 44);
+		painelBotoes.add(btnNovo);
+		
+		JButton btnEditar = new JButton("");
+		btnEditar.setIcon(new ImageIcon(FrmAgenda.class.getResource("/br/senai/sp/jandira/imagens/editar32.png")));
+		btnEditar.setBounds(64, 11, 44, 44);
+		painelBotoes.add(btnEditar);
+		
+		JButton btnExcluir = new JButton("");
+		btnExcluir.setIcon(new ImageIcon(FrmAgenda.class.getResource("/br/senai/sp/jandira/imagens/excluir32.png")));
+		btnExcluir.setBounds(118, 11, 44, 44);
+		painelBotoes.add(btnExcluir);
+		
+		JButton btnSair = new JButton("");
+		btnSair.setIcon(new ImageIcon(FrmAgenda.class.getResource("/br/senai/sp/jandira/imagens/sair32.png")));
+		btnSair.setBounds(360, 11, 44, 44);
+		painelBotoes.add(btnSair);
 		
 		criarTabela();
 		
@@ -92,7 +122,7 @@ public class FrmAgenda extends JFrame {
 		scrollTabela.setViewportView(tabelaContatos);
 		
 		scrollTabela = new JScrollPane();
-		scrollTabela.setBounds(10, 24, 414, 145);
+		scrollTabela.setBounds(10, 24, 394, 145);
 		painelTabela.add(scrollTabela);
 	}
 }
